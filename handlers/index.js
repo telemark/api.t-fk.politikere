69c8dbfd1baaca681ec76e9c18ca5bef9ccd9546
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-04-30 06:59:41 +0200

    standard --fix (patch)

--- handlers/index.js
+++ handlers/index.js
@@ -14,7 +14,7 @@ function getPoliticians (request, reply) {
 }
 
 function searchPoliticians (request, reply) {
-  politicians.find({'$text': {'$search': request.params.searchText}},
+  politicians.find({ '$text': { '$search': request.params.searchText } },
     function findPoliticiansBySearch (error, data) {
       reply(error || data)
     }
@@ -23,7 +23,7 @@ function searchPoliticians (request, reply) {
 
 function getPolitician (request, reply) {
   var pID = parseInt(request.params.politicianID, 10)
-  politicians.find({'recno': pID},
+  politicians.find({ 'recno': pID },
     function findPolitician (error, data) {
       reply(error || data)
     }
@@ -32,50 +32,50 @@ function getPolitician (request, reply) {
 
 function getParties (request, reply) {
   politicians.aggregate([
-    {'$unwind': '$committees'},
-      {'$match': {'committees.role': 'Parti'}},
-    {'$group': {
+    { '$unwind': '$committees' },
+    { '$match': { 'committees.role': 'Parti' } },
+    { '$group': {
       '_id': '$committees.groupRecno',
-      'name': {'$first': '$committees.name'},
-      'address': {'$first': '$committees.address'},
-      'place': {'$first': '$committees.place'},
-      'zip': {'$first': '$committees.zip'},
-      'mail': {'$first': '$committees.mail'},
-      'tlf': {'$first': '$committees.tlf'},
-      'url': {'$first': '$committees.url'}
-    }}
+      'name': { '$first': '$committees.name' },
+      'address': { '$first': '$committees.address' },
+      'place': { '$first': '$committees.place' },
+      'zip': { '$first': '$committees.zip' },
+      'mail': { '$first': '$committees.mail' },
+      'tlf': { '$first': '$committees.tlf' },
+      'url': { '$first': '$committees.url' }
+    } }
   ],
-    function aggregatePartiesResult (error, data) {
-      reply(error || data)
-    }
+  function aggregatePartiesResult (error, data) {
+    reply(error || data)
+  }
   )
 }
 
 function getParty (request, reply) {
   var pID = parseInt(request.params.partyID, 10)
   politicians.aggregate([
-      {'$unwind': '$committees'},
-      {'$match': {'committees.groupRecno': pID}},
-    {'$group': {
+    { '$unwind': '$committees' },
+    { '$match': { 'committees.groupRecno': pID } },
+    { '$group': {
       '_id': '$committees.groupRecno',
-      'name': {'$first': '$committees.name'},
-      'address': {'$first': '$committees.address'},
-      'place': {'$first': '$committees.place'},
-      'zip': {'$first': '$committees.zip'},
-      'mail': {'$first': '$committees.mail'},
-      'tlf': {'$first': '$committees.tlf'},
-      'url': {'$first': '$committees.url'}
-    }}
+      'name': { '$first': '$committees.name' },
+      'address': { '$first': '$committees.address' },
+      'place': { '$first': '$committees.place' },
+      'zip': { '$first': '$committees.zip' },
+      'mail': { '$first': '$committees.mail' },
+      'tlf': { '$first': '$committees.tlf' },
+      'url': { '$first': '$committees.url' }
+    } }
   ],
-    function aggregatePartyResult (error, data) {
-      reply(error || data)
-    }
+  function aggregatePartyResult (error, data) {
+    reply(error || data)
+  }
   )
 }
 
 function getPartyMembers (request, reply) {
   var pID = parseInt(request.params.partyID, 10)
-  politicians.find({'committees.groupRecno': pID},
+  politicians.find({ 'committees.groupRecno': pID },
     function findPartyMembers (error, data) {
       reply(error || data)
     }
@@ -84,50 +84,50 @@ function getPartyMembers (request, reply) {
 
 function getCommittees (request, reply) {
   politicians.aggregate([
-      {'$unwind': '$committees'},
-      {'$match': {'committees.role': {'$ne': 'Parti'}}},
-    {'$group': {
+    { '$unwind': '$committees' },
+    { '$match': { 'committees.role': { '$ne': 'Parti' } } },
+    { '$group': {
       '_id': '$committees.groupRecno',
-      'name': {'$first': '$committees.name'},
-      'address': {'$first': '$committees.address'},
-      'place': {'$first': '$committees.place'},
-      'zip': {'$first': '$committees.zip'},
-      'mail': {'$first': '$committees.mail'},
-      'tlf': {'$first': '$committees.tlf'},
-      'url': {'$first': '$committees.url'}
-    }}
+      'name': { '$first': '$committees.name' },
+      'address': { '$first': '$committees.address' },
+      'place': { '$first': '$committees.place' },
+      'zip': { '$first': '$committees.zip' },
+      'mail': { '$first': '$committees.mail' },
+      'tlf': { '$first': '$committees.tlf' },
+      'url': { '$first': '$committees.url' }
+    } }
   ],
-    function aggregateCommitteesResult (error, data) {
-      reply(error || data)
-    }
+  function aggregateCommitteesResult (error, data) {
+    reply(error || data)
+  }
   )
 }
 
 function getCommittee (request, reply) {
   var cID = parseInt(request.params.committeeID, 10)
   politicians.aggregate([
-      {'$unwind': '$committees'},
-      {'$match': {'committees.groupRecno': cID}},
-    {'$group': {
+    { '$unwind': '$committees' },
+    { '$match': { 'committees.groupRecno': cID } },
+    { '$group': {
       '_id': '$committees.groupRecno',
-      'name': {'$first': '$committees.name'},
-      'address': {'$first': '$committees.address'},
-      'place': {'$first': '$committees.place'},
-      'zip': {'$first': '$committees.zip'},
-      'mail': {'$first': '$committees.mail'},
-      'tlf': {'$first': '$committees.tlf'},
-      'url': {'$first': '$committees.url'}
-    }}
+      'name': { '$first': '$committees.name' },
+      'address': { '$first': '$committees.address' },
+      'place': { '$first': '$committees.place' },
+      'zip': { '$first': '$committees.zip' },
+      'mail': { '$first': '$committees.mail' },
+      'tlf': { '$first': '$committees.tlf' },
+      'url': { '$first': '$committees.url' }
+    } }
   ],
-    function aggregateCommitteeResult (error, data) {
-      reply(error || data)
-    }
+  function aggregateCommitteeResult (error, data) {
+    reply(error || data)
+  }
   )
 }
 
 function getCommitteeMembers (request, reply) {
   var cID = parseInt(request.params.committeeID, 10)
-  politicians.find({'committees.groupRecno': cID},
+  politicians.find({ 'committees.groupRecno': cID },
     function findCommitteeMembersResult (error, data) {
       reply(error || data)
     }
